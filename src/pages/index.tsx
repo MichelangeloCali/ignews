0a5ec969fe4775bd4ba150a51d63bafd1865b08b
michelangelo <michelangelocali@hotmail.com>
2023-02-05 12:18:59 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Home
