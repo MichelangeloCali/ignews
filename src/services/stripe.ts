d86ec7714e368b664f00791239ce27e100a4a4a6
michelangelo <michelangelocali@hotmail.com>
2023-02-11 21:28:50 -0300

    feat: callback signIn

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,11 +1,10 @@
 import Stripe from 'stripe'
-import { version } from '../../package.json'
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   // @ts-ignore stripe-version-2022-08-01
   apiVersion: '2020-08-27',
   appInfo: {
     name: 'Ignews',
-    version,
+    version: '0.1.0',
   },
 })
