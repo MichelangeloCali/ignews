0528df93c7ecc3ed8eded6821d53c2e8f2e3673a
michelangelo <michelangelocali@hotmail.com>
2023-02-13 20:39:32 -0300

    feat: stripe redirection service

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/react'
+import { api } from '@/services/api'
+import { getStripeJs } from '@/services/stripe-js'
 import styles from './SubscribeButton.module.scss'
 
 interface SubscribeButtonProps {
@@ -8,15 +10,34 @@ interface SubscribeButtonProps {
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   const { data: session } = useSession()
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn('github')
       return
     }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch (err) {
+      if (err instanceof Error) {
+        alert(err.message)
+      } else {
+        alert('Ocorreu um erro desconhecido')
+      }
+    }
   }
 
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Inscreva-se
     </button>
   )
